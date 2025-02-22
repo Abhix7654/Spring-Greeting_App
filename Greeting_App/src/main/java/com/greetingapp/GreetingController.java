c185858b6b7aa39e141af765ceaca41d0c1dfbd5
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-02-22 13:36:48 +0530

    [Abhishek] add. Greeting App Ability for the Greeting App to Edit a Greeting Messages in the Repository

--- Greeting_App/src/main/java/com/greetingapp/GreetingController.java
+++ Greeting_App/src/main/java/com/greetingapp/GreetingController.java
@@ -39,4 +39,8 @@ public class GreetingController {
     public List<Greeting> getAllGreetings() {
         return greetingService.getAllGreetings();
     }
+    @PutMapping("/update/{id}")
+    public Greeting updateGreeting(@PathVariable Long id, @RequestBody Greeting updatedGreeting) {
+        return greetingService.updateGreeting(id, updatedGreeting);
+    }
 }
